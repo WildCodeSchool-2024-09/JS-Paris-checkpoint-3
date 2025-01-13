bda0fcbcdeb3cc5daf452f2a05fae9a5dc01e482
Lassana88 <lassana2488@gmail.com>
2025-01-13 11:28:45 +0100

    finished run test step2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,15 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tiles = await tileRepository.readAll();
+    res.json(tiles);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des tuiles" });
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
