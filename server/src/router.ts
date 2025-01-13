664648462cd1e341bb082cdcf01c137021bb1e1b
Karim Ben_Harzallah <karim.benharzallah@hotmail.fr>
2025-01-13 10:24:16 +0100

    step2 finish

--- server/src/router.ts
+++ server/src/router.ts
@@ -11,6 +11,8 @@ import boatActions from "./modules/boat/boatActions";
 router.get("/api/boats", boatActions.browse);
 
 import gameActions from "./modules/game/gameActions";
+import tileActions from "./modules/tile/tileActions";
+router.get("/api/tiles", tileActions.browse);
 
 router.post("/api/games", gameActions.add);
 
