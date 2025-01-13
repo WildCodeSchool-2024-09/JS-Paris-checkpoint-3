6f9117f34ecba362fbed73bd72d925ae53e2fe6b
Sidi <sidimed.sidimed.147@gmail.com>
2025-01-13 12:28:08 +0100

    finishing checkpoint 3

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -3,23 +3,44 @@ import type { RequestHandler } from "express";
 import boatRepository from "./boatRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  try {
-    // Fetch all boats from the database
-    const boats = await boatRepository.readAll();
+	try {
+		// Fetch all boats from the database
+		const boats = await boatRepository.readAll(req.query.name);
 
-    // Respond with the boats in JSON format
-    res.json(boats);
-  } catch (err) {
-    // Pass any errors to the error-handling middleware
-    next(err);
-  }
+		// Respond with the boats in JSON format
+		res.json(boats);
+	} catch (err) {
+		// Pass any errors to the error-handling middleware
+		next(err);
+	}
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+	// your code here
+	try {
+		const boat = {
+			id: Number(req.params.id),
+			name: req.body.name,
+			coord_x: req.body.coord_x,
+			coord_y: req.body.coord_y,
+		};
+
+		const affectedRows = await boatRepository.update(boat);
+
+		// If the category is not found, respond with HTTP 404 (Not Found)
+		// Otherwise, respond with the category in JSON format
+		if (affectedRows === 0) {
+			res.sendStatus(404);
+		} else {
+			res.sendStatus(204);
+		}
+	} catch (err) {
+		// Pass any errors to the error-handling middleware
+		next(err);
+	}
 };
 
 export default {
-  browse,
-  edit,
+	browse,
+	edit,
 };
--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,14 +1,37 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+	try {
+		// Fetch all boats from the database
+		const tiles = await tileRepository.readAll();
+
+		// Respond with the boats in JSON format
+		res.json(tiles);
+	} catch (err) {
+		// Pass any errors to the error-handling middleware
+		next(err);
+	}
 };
 
 const validate: RequestHandler = async (req, res, next) => {
-  // your code here
+	// your code here
+	try {
+		const tilesarray = await tileRepository.readByCoordinates(
+			Number(req.body.coord_x),
+			Number(req.body.coord_y),
+		);
+		if (tilesarray.length > 0) {
+			next();
+		} else {
+			res.sendStatus(422);
+		}
+	} catch (error) {
+		next(error);
+	}
 };
 
 export default {
-  browse,
-  validate,
+	browse,
+	validate,
 };
