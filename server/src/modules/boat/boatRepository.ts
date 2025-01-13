4798a9708a1591eb72215e1ae24870c787ad2381
Julien <juju.gruet@mail.com>
2025-01-13 10:56:42 +0100

    test3

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -13,7 +13,7 @@ class BoatRepository {
   async readAll(where = {}) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x"
+      "select * from boat order by coord_y, coord_x",
     );
 
     // Return the array of tiles
@@ -29,7 +29,7 @@ class BoatRepository {
 
     const [result] = await databaseClient.query<Result>(
       "UPDATE boat SET coord_x = ?, coord_y = ? WHERE id = ?",
-      [coord_x, coord_y, id]
+      [coord_x, coord_y, id],
     );
 
     return result.affectedRows;
