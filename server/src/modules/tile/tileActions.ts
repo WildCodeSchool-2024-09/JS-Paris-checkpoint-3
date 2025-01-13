924580a67ac41e44c8af1af0700c87b23071951b
Marina <98844023+MarinaMassot@users.noreply.github.com>
2025-01-13 09:50:31 +0100

    npm run test step2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,15 @@
 import type { RequestHandler } from "express";
 
+import tileRepository from "./tileRepository";
+
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tiles = await tileRepository.readAll();
+    res.json(tiles);
+    // your code here
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
